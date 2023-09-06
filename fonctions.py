42be47b28fe21a75a471156cdcfee1868f39ba71
PierreHeb <pierre.hebert1@protonmail.ch>
2023-09-06 17:17:07 +0200

    Update des fichiers .py

--- fonctions.py
+++ fonctions.py
@@ -1,5 +1,7 @@
 def puissance(a,b):
-	var = a**b
+	var = a
+	for i in range(1,b):
+	   var *= a
 	return var
 	if not type(var) is int:
 		raise TypeError("Only integers are allowed")
--- test.py
+++ test.py
@@ -9,5 +9,7 @@ def test_1( ) :
 def test_2():
 	assert f.puissance(-2,3) == -8
 	assert f.puissance(-2,2) == 4
-	
+	assert f.puissance (0,0) == 1
+	assert f.puissance (0,3) == 0
+	assert f.puissance (0,-3) == 0
 
