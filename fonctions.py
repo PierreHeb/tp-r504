5bdb87af1defa489fc88542aa9a99d3de55223cb
PierreHeb <pierre.hebert1@protonmail.ch>
2023-09-06 15:36:22 +0200

    Nouveaux fichiers et actualisation de prog.py

---
+++ fonctions.py
@@ -0,0 +1,7 @@
+def puissance(a,b):
+	var = a**b
+	return var
+	if not type(var) is int:
+		raise TypeError("Only integers are allowed")
+	
+	
--- prog1.py
+++ prog1.py
@@ -1,1 +1,20 @@
+import fonctions as f
+
+
+
 print("Hello, World!")
+
+
+"""while(True):
+	def nbcarre(nombre):
+		print(nombre**2)
+		
+		
+	nombreChoisi = int(input("Choisir un nombre : "))
+	nbcarre(nombreChoisi)
+"""
+	
+	
+res = f.puissance(2,5)
+
+print(res)
