cc65c056daaf39dbf276de1960c992c806c0e233
PierreHeb <pierre.hebert1@protonmail.ch>
2023-09-28 15:08:57 +0200

    Fin du tp v2

--- fonctions.py
+++ fonctions.py
@@ -3,31 +3,24 @@ def puissance(a,b):
 		raise TypeError("Only integers are allowed")
 	if not type(b) is int:
 		raise TypeError("Only integers are allowed")
-	
+	if a == 0 and b < 0:
+		raise ValueError("La puissance de 0 ne peut pas être défini par un nombre négatif")
+		
+		
 	var = 1
+	for i in range(b):
+	    var = var*a
+	    print(var)
 	if b<0:
-	 b = b*-1
-	 for i in range(b):
-	    var = var*a
-	    print(var)
 	 return 1/var
-	
-	   
+	 
 	else:
-	 for i in range(b):
-	    var = var*a
-	    print(var)
 	 return var
 	
-	if not type(var) is int:
-		raise TypeError("Only integers are allowed")
-		
-	if a == 0 and b < 0:
-		raise TypeError("La puissance de 0 ne peut pas être défini par un nombre négatif")
+	
 	
 	
 
-print(puissance(2,-3))
 	
 	
 		
--- test.py
+++ test.py
@@ -19,3 +19,9 @@ def test_3():
 	assert f.puissance(1,-2) == 1
 	assert f.puissance(2,2) == 4
 	assert f.puissance(2,-3) == 0.125
+	
+	
+	
+def test_func():
+  with pytest.raises(ValueError, match="La puissance de 0 ne peut pas être défini par un nombre négatif"):
+    f.puissance(0,-3)
