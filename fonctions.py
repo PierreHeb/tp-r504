bd0c9e3fc4ae737c2e180536c793afe860642dc5
PierreHeb <pierre.hebert1@protonmail.ch>
2023-09-28 15:43:05 +0200

    Fin du tp v3

--- fonctions.py
+++ fonctions.py
@@ -6,19 +6,23 @@ def puissance(a,b):
 	if a == 0 and b < 0:
 		raise ValueError("La puissance de 0 ne peut pas être défini par un nombre négatif")
 		
-		
 	var = 1
-	for i in range(b):
-	    var = var*a
-	    print(var)
 	if b<0:
+	 b = b*(-1)
+	 for i in range(b):
+	     var = var*a
+	     print(var)
 	 return 1/var
 	 
 	else:
+	 for i in range(b):
+	     var = var*a
+	     print(var)
 	 return var
 	
 	
-	
+
+
 	
 
 	
